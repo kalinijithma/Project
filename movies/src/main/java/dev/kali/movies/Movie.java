1ec94612032cb1925c82a90ea934ec19f111308e
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2024-11-03 00:11:01 +0530

    added

---
+++ movies/src/main/java/dev/kali/movies/Movie.java
@@ -0,0 +1,24 @@
+package dev.kali.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private  String imdbID;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+}
