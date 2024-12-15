cb7ab12fd67897b3523ccf7da25c3502561528d6
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2024-12-15 23:34:41 +0530

    updated

---
+++ movies/src/main/java/dev/kali/movies/MovieController.java
@@ -0,0 +1,17 @@
+package dev.kali.movies;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public ResponseEntity<String> getaAllMovies() {
+
+        return new ResponseEntity<String >("All Movies!", HttpStatus.OK);
+    }
+}
